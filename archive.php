db9ae19aac641d657d29eca6b8257ae643883ee1
Yochi <yochisstory@gmail.com>
2018-02-07 01:25:02 +0900

    Delete .wrap container

--- archive.php
+++ archive.php
@@ -9,50 +9,48 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+	<?php
+	if ( have_posts() ) : ?>
+
+		<header class="page-header">
+		<?php
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="archive-description">', '</div>' );
+		?>
+		</header><!-- .page-header -->
 
 		<?php
-		if ( have_posts() ) : ?>
+		/* Start the Loop */
+		while ( have_posts() ) : the_post();
 
-			<header class="page-header">
-			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-			?>
-			</header><!-- .page-header -->
+			/*
+			 * Include the Post-Format-specific template for the content.
+			 * If you want to override this in a child theme, then include a file
+			 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+			 */
+			get_template_part( 'template-parts/content', 'excerpt' );
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+		/* End the loop */
+		endwhile;
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'excerpt' );
+		/* Previous/next page navigation */
+		the_posts_pagination( array(
+			'prev_text'          => esc_html( '&laquo; ' ) . __( 'Previous page', 'dan' ),
+			'next_text'          => __( 'Next page', 'dan' ) . esc_html( ' &raquo;' ),
+			'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'dan' ) . '</span>',
+		) );
 
-			/* End the loop */
-			endwhile;
+	else :
 
-			/* Previous/next page navigation */
-			the_posts_pagination( array(
-				'prev_text'          => esc_html( '&laquo; ' ) . __( 'Previous page', 'dan' ),
-				'next_text'          => __( 'Next page', 'dan' ) . esc_html( ' &raquo;' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'dan' ) . '</span>',
-			) );
+		get_template_part( 'template-parts/content', 'none' );
 
-		else :
+	endif; ?>
 
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
-</div><!-- .wrap -->
-
-<?php get_footer();
+	</main><!-- #main -->
+</div><!-- #primary -->
+<?php
+get_sidebar();
+get_footer();
--- front-page.php
+++ front-page.php
@@ -14,44 +14,42 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
 
-			<?php
-			// Show the selected frontpage content.
-			if ( have_posts() ) :
-				while ( have_posts() ) : the_post();
-					get_template_part( 'template-parts/content', 'front-page' );
-				endwhile;
-			else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
-				get_template_part( 'template-parts/content', 'none' );
-			endif; ?>
+		<?php
+		// Show the selected frontpage content.
+		if ( have_posts() ) :
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', 'front-page' );
+			endwhile;
+		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
+			get_template_part( 'template-parts/content', 'none' );
+		endif; ?>
 
-			<?php
-			// Get each of our panels and show the post data.
-			if ( 0 !== dan_panel_count() || is_customize_preview() ) : // If we have pages to show.
+		<?php
+		// Get each of our panels and show the post data.
+		if ( 0 !== dan_panel_count() || is_customize_preview() ) : // If we have pages to show.
 
-				/**
-				 * Filter number of front page sections in Dan.
-				 *
-				 * @since Twenty Seventeen 1.0
-				 *
-				 * @param int $num_sections Number of front page sections.
-				 */
-				$num_sections = apply_filters( 'dan_front_page_sections', 4 );
-				global $dan_counter;
+			/**
+			 * Filter number of front page sections in Dan.
+			 *
+			 * @since Twenty Seventeen 1.0
+			 *
+			 * @param int $num_sections Number of front page sections.
+			 */
+			$num_sections = apply_filters( 'dan_front_page_sections', 4 );
+			global $dan_counter;
 
-				// Create a setting and control for each of the sections available in the theme.
-				for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-					$dan_counter = $i;
-					dan_front_page_section( null, $i );
-				}
+			// Create a setting and control for each of the sections available in the theme.
+			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
+				$dan_counter = $i;
+				dan_front_page_section( null, $i );
+			}
 
-			endif; ?>
+		endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer();
