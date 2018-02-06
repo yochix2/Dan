c872a323ee86fee75ca3beda89081d88f577dc6c
Yochi <yochisstory@gmail.com>
2018-02-07 01:32:44 +0900

    Delete .widget-area-inner

--- sidebar.php
+++ sidebar.php
@@ -13,7 +13,5 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary">
-	<div class="widget-area-inner">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</div><!-- .widget-area-inner -->
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
