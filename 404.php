4c7c8ee17314f293f1b82322deb4c8b8c9d76d75
Yochi <yochisstory@gmail.com>
2018-02-07 01:23:03 +0900

    Fix .page-title

--- 404.php
+++ 404.php
@@ -9,23 +9,21 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'dan' ); ?></h1>
-				</header><!-- .page-header -->
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<section class="error-404 not-found">
+			<header class="page-header">
+				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'dan' ); ?></h1>
+			</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'dan' ); ?></p>
+			<div class="page-content">
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'dan' ); ?></p>
 
-					<?php get_search_form(); ?>
+				<?php get_search_form(); ?>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
+			</div><!-- .page-content -->
+		</section><!-- .error-404 -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer();
